032b74d68432eb84af6862aadb6d47c21416d523
xudongdev <me@xudong.dev>
2020-02-25 11:49:33 +0800

    use typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
+import typescript from "rollup-plugin-ts";
 import pkg from "./package.json";
 
 export default [
   {
-    input: "src/useQuery.js",
+    input: "src/index.ts",
+    plugins: [typescript({ transpiler: "babel" })],
     external: ["qs", "react", "react-router-dom"],
     output: [
       { file: pkg.main, format: "cjs" },
